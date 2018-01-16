f3a9aaf54a1f14581fa17183de3ae8e513d98a90
Eric Roth <sirviver@gmail.com>
2018-01-16 09:15:16 -0600

    added script to package.json, added error to .listen

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
 const express = require( 'express' );
 const app = express();
+// console.log(express())
 
-app.listen(3000, () => console.log("Server running"))
+
+
+app.use('/', function (req, res, next) {
+  console.log(req.method, req.path, res.statusCode)
+
+  next();
+
+  // do your logging here
+  // call `next`, or else your app will be a black hole — receiving requests but never properly responding
+})
+
+app.use('/news', function (req, res, next) {
+  console.log("request from /news")
+  // console.log(req.method, req.path, res.statusCode)
+
+  next();
+})
+
+
 
 app.get('/', function(req, res, next) {
   res.send("<h1> get working </h1>")
+})
+
+app.get('/news', function(req, res, next) {
+  res.send("<h2> news yo </h2>")
+})
+
+app.listen(3000, (err) => {
+  if(err) throw err;
+  console.log("Server running")
 })
